1f04a8eb51b72f37a12c6f811facb3b28f62323f
Xernnn <std.sonnguyen@gmail.com>
2024-03-14 14:15:12 +0700

    properer commit message

--- prac1/mlmed_practical1.py
+++ prac1/mlmed_practical1.py
@@ -7,24 +7,89 @@ Original file is located at
     https://colab.research.google.com/drive/1sRsXC_RejuBd_-kpz-_sBbopNSMH37-2
 
 # **1. Prepare the Data**
-
-I downloaded the dataset directly from Kaggle so that it wouldn't be a hassle to upload the data over and over.
 """
 
-! pip install -q kaggle
-! kaggle datasets list
+from google.colab import drive
+drive.mount('/content/drive')
 
 """**The dataset:** https://www.kaggle.com/datasets/shayanfazeli/heartbeat?resource=download"""
 
-!kaggle datasets download -d shayanfazeli/heartbeat
+import pandas as pd
 
-!unzip "heartbeat.zip"
+train_path = '/content/drive/My Drive/ECGHeartBeat/mitbih_train.csv'
+test_path = '/content/drive/My Drive/ECGHeartBeat/mitbih_test.csv'
+train = pd.read_csv(train_path, header=None)
+test = pd.read_csv(test_path, header=None)
 
-"""#**2. Work with the dataset**
+"""#**2. Preprocess the Data**
 
-Like I don't actually know how to do this so here's a cat instead.
 
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+
+x_train = train.drop(187, axis=1)
+y_train = train[187]
+x_test = test.drop(187, axis=1)
+y_test = test[187]
+
+print(x_train, y_train, x_test, y_test)
+
+"""#**3. Exploratory Data Analysis (EDA)**"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.hist(y_train, bins=np.arange(min(y_train), max(y_train)+2)-0.5, edgecolor='black')
+plt.xlabel("Classes")
+plt.ylabel("Count")
+plt.xticks(range(len(set(y_train))))
+plt.show()
+
+"""#**4. Train the Model**"""
+
+from sklearn.ensemble import RandomForestClassifier
+
+def train_model(x_train, y_train):
+    model = RandomForestClassifier(n_estimators=100, random_state=1)
+    model.fit(x_train, y_train)
+    return model
+
+model = train_model(x_train, y_train)
+
+"""#**5. Evaluate the Model**"""
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+def evaluate_model(model, x_test, y_test):
+    y_pred = model.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+    print(classification_report(y_test, y_pred))
+    return y_pred
+
+def plot_confusion_matrix(y_test, y_pred, class_names):
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion Matrix')
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names)
+    plt.yticks(tick_marks, class_names)
+    plt.xlabel('Predicted Class')
+    plt.ylabel('True Class')
+
+    thresh = cm.max() / 2.
+    for i, j in np.ndindex(cm.shape):
+        plt.text(j, i, format(cm[i, j], 'd'),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.show()
+
+y_pred = evaluate_model(model, x_test, y_test)
+plot_confusion_matrix(y_test, y_pred, class_names=[0, 1, 2, 3, 4])
+
+"""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠈⠻⣮⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡿⠋⠀⠀⠀⠀⠙⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠿⠿⣿⣷⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
